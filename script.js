6d7bf9a4d1bbab2a8e5b15f26f8b36da3dfc812e
ParkertheDeveloper <parkerdhugo@gmail.com>
2026-09-12 23:54:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+// =========================
+// VOTE BUTTON
+// =========================
 
+const voteButton = document.querySelector(".vote-button");
+
+voteButton.addEventListener("click", function () {
+
+    alert("Voting system coming soon!");
+
+});
+
+
+// =========================
+// RANKING CARD CLICK
+// =========================
+
+const rankingCards = document.querySelectorAll(".ranking-card");
+
+rankingCards.forEach(function(card) {
+
+    card.addEventListener("click", function() {
+
+        const person = card.querySelector("h3").textContent;
+
+        alert("You selected " + person);
+
+    });
+
+});
